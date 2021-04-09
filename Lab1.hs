73bc16fca43b54ebef8fdf955dbaaa76e823c5cd
Tom Brander <tom.e.brander@gmail.com>
2021-04-09 09:32:52 +0200

    Initial commit with current progress

---
+++ Lab1.hs
@@ -0,0 +1,40 @@
+import Data.List
+import System.IO
+
+test1 = [x*(-1)^x | x <- [1..100]]
+test2 = [24,-11,-34,42,-24,7,-19,21]
+--Returns the sum of all elements in the input list
+listSum :: [Int]->Int
+listSum [] = 0
+listSum (x:xs) = x + listSum xs
+
+--Returns the index pair which results the minimum sum of 2 pairs in the array
+minOfTwoArrays:: (Int,Int)->(Int,Int)->[Int]->(Int,Int)
+minOfTwoArrays a b array
+    | listSum ( drop (fst a) (take ( (snd a) + 1) array) ) < listSum ( drop (fst b) (take ( (snd b) + 1) array) )     = a
+    | otherwise                 = b
+
+--Removes one tuple of a tuples list
+removeItem :: (Int,Int)->[(Int,Int)]->[(Int,Int)]
+removeItem _ [] = []
+removeItem x (y:ys) |x==y       =removeItem x ys
+                    |otherwise  =y : removeItem x ys
+
+--Returns all the possible subarrays in form of tuple pairs of index i and j
+subArrays :: Int->Int->[Int]->[(Int,Int)] --subArrays(i,j, array) -> all subArrays indexes
+subArrays i j array 
+    | array==[]                                             = []
+    | j < (length array) - 1                                = [(i, j)] ++ subArrays i (j+1) array
+    | j >= (length array) - 1 && i <= (length array) - 1    =  [(i, j) ] ++ subArrays (i+1) (i+1) array
+    |otherwise                                              = []
+
+--Find the tuple pair which returns the smallest sum in the array
+findMin :: [(Int,Int)]->[Int]->(Int,Int) -- FindMin(pairs of tuples (i,j), array) -> the tuples which return smallest sum
+findMin [] array = (0,0)
+findMin (x:xs) array = minOfTwoArrays x (findMin xs array) array
+
+--Returns the k smallest tuple pairs which returns the smallest sums
+kSmallest :: [(Int,Int)]->[Int]->Int->[(Int,Int)] --kSmallest(all possibles subarrays index, array, k) -> List of tuples: (size, sublist, i, j)
+kSmallest subArrs array k
+    |k>0        = [findMin subArrs array] ++ kSmallest (removeItem (findMin subArrs array) subArrs) array (k-1)
+    |otherwise  = []
