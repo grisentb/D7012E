741a8b9152117418fb886d618bd1606242beee07
Grisen <tom.e.brander@gmail.com>
2021-04-09 13:13:29 +0200

    Lab1 fixed & completed

--- Lab1.hs
+++ Lab1.hs
@@ -1,6 +1,8 @@
 import Data.List
 import System.IO
 
+test1 :: [Int]
+test2 :: [Int]
 test1 = [x*(-1)^x | x <- [1..100]]
 test2 = [24,-11,-34,42,-24,7,-19,21]
 --Returns the sum of all elements in the input list
@@ -25,16 +27,24 @@ subArrays :: Int->Int->[Int]->[(Int,Int)] --subArrays(i,j, array) -> all subArra
 subArrays i j array 
     | array==[]                                             = []
     | j < (length array) - 1                                = [(i, j)] ++ subArrays i (j+1) array
-    | j >= (length array) - 1 && i <= (length array) - 1    =  [(i, j) ] ++ subArrays (i+1) (i+1) array
+    | j >= (length array) - 1 && i <= (length array) - 1    = [(i, j) ] ++ subArrays (i+1) (i+1) array
     |otherwise                                              = []
 
 --Find the tuple pair which returns the smallest sum in the array
-findMin :: [(Int,Int)]->[Int]->(Int,Int) -- FindMin(pairs of tuples (i,j), array) -> the tuples which return smallest sum
-findMin [] array = (0,0)
-findMin (x:xs) array = minOfTwoArrays x (findMin xs array) array
+findMinSubArray :: [(Int,Int)]->[Int]->(Int,Int) -- findMinSubArray(pairs of tuples (i,j), array) -> the tuples which return smallest sum
+findMinSubArray [] array = (0,0)
+findMinSubArray (x:xs) array = minOfTwoArrays x (findMinSubArray xs array) array
+
+formatConversion :: [(Int,Int)]->[Int]->[((Int,Int), [Int])]
+formatConversion [] array = []
+formatConversion (x:xs) array = [(x,drop (fst x) (take ((snd x) + 1) array) )] ++ formatConversion xs array
 
 --Returns the k smallest tuple pairs which returns the smallest sums
-kSmallest :: [(Int,Int)]->[Int]->Int->[(Int,Int)] --kSmallest(all possibles subarrays index, array, k) -> List of tuples: (size, sublist, i, j)
-kSmallest subArrs array k
-    |k>0        = [findMin subArrs array] ++ kSmallest (removeItem (findMin subArrs array) subArrs) array (k-1)
+getKSmallestIndexPairs:: [(Int,Int)]->[Int]->Int->[(Int,Int)] --kSmallest(all possibles subarrays index, array, k) -> List of tuples: (size, sublist, i, j)
+getKSmallestIndexPairs subArrs array k
+    |k>0        = [findMinSubArray subArrs array] ++ getKSmallestIndexPairs(removeItem (findMinSubArray subArrs array) subArrs) array (k-1)
     |otherwise  = []
+
+--Trying to make the getKSmallestIndexPairscall prettier, calls kSmallest
+kSmallestSubList :: [Int]->Int->[((Int,Int),[Int])]
+kSmallestSubList array k = formatConversion (getKSmallestIndexPairs(subArrays 0 0 array) array k) array
